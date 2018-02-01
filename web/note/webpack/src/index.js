d8542fb638fa022b211df6e69e2c23f6ff28ef0e
wangxiong1004 <77302232@qq.com>
2018-02-01 23:06:50 +0800

    webpack

--- web/note/webpack/src/index.js
+++ web/note/webpack/src/index.js
@@ -3,12 +3,19 @@
 import _ from "lodash";
 import printMe from './print.js';
 
+import { cube } from './math.js';
+
+import './index.css';
+
+if (process.env.NODE_ENV !== 'production') {
+	console.log('Looks like me are in development mode!');
+}
 
 function component() {
   var element = document.createElement("div");
   var btn = document.createElement('button');
 
-  element.innerHTML = _.join(["hello", "webpack"], " ");
+  element.innerHTML = _.join(["hello", "webpack", "5 cubed is equal to " + cube(5)], " ");
   btn.innerHTML = 'Click me and check the console!';
   btn.onclick = printMe;
   element.appendChild(btn);
@@ -16,4 +23,16 @@ function component() {
   return element;
 }
 
-document.body.appendChild(component());
+// document.body.appendChild(component());
+let element = component();  // 当 print.js 改变导致页面重新渲染时，重新获取渲染的元素
+document.body.appendChild(element);
+
+if (module.hot) {
+	module.hot.accept('./print.js', function() {
+	  console.log('Accepting the updated printMe module!');
+	  // printMe();
+	  document.body.removeChild(element);
+	  element = component(); // 重新渲染页面后，component 更新 click 事件处理
+	  document.body.appendChild(element);
+	});
+}
--- web/note/webpack/webpack.config.js
+++ web/note/webpack/webpack.config.js
@@ -1,24 +1,37 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const webpack = require('webpack');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
   entry: {
-    app: './src/index.js',
-    print: './src/print.js'
+    app: './src/index.js'
+    // print: './src/print.js'
   },
   output: {
     filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/'
+  },
+  module: {
+    rules: [{
+      test: /\.css$/,
+      use: ['style-loader', 'css-loader']
+    }]
   },
   plugins: [
     new CleanWebpackPlugin(['dist']),
     new HtmlWebpackPlugin({
       title: 'Output Management'
-    })
+    }),
+    new webpack.NamedModulesPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new UglifyJSPlugin()
   ],
   devtool: 'inline-source-map',
   devServer: {
-    contentBase: './dist'
+    contentBase: './dist',
+    hot: true
   }
 };
